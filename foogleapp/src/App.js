80febf28db46fe5c137d27774eb7cfbdb229002b
Hamza Warsame <hamza360123@gmail.com>
2023-01-23 09:26:23 +0300

    create app and config tailwind

---
+++ foogleapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <div className>
+      <h1>Foogle</h1>
+    </div>
+  );
+}
+
+export default App;
